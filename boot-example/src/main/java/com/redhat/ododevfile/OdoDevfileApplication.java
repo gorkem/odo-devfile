463dad485bd4f3e338fe73b2e46d3d50e3f65eb9
Gorkem Ercan <gorkem.ercan@gmail.com>
2020-06-09 10:40:45 -0400

    add pyton nodejs and spring boot

---
+++ boot-example/src/main/java/com/redhat/ododevfile/OdoDevfileApplication.java
@@ -0,0 +1,13 @@
+package com.redhat.ododevfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdoDevfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdoDevfileApplication.class, args);
+	}
+
+}
---
+++ boot-example/src/test/java/com/redhat/ododevfile/OdoDevfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhat.ododevfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdoDevfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
